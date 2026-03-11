ec5924830bd1c1d898606915de57032cf58655f5
Satyam Jha <jhaa77313@gmail.com>
2026-03-11 15:44:17 +0530

    Update route to match all subpaths for index.html

--- Backend/server.js
+++ Backend/server.js
@@ -204,7 +204,7 @@ const completion = await client.chat.completions.create({
 });
 app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/*", (req, res) => {
+app.get("/./*", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 // ================= START SERVER =================
